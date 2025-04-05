f04897204df609feb94373d938255cb973ab944b
felipematheus1337 <lipehbr@gmail.com>
2025-04-05 17:34:31 -0300

    created user entity

---
+++ Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todolist
+{
+
+    enum TaskStatus
+    {
+        PENDING, FINISHED
+    }
+    class Tarefa
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public DateTime creationDate { get; set; }
+
+        public TaskStatus status;
+
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace todolist
+{
+
+ 
+    class User
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public List<Tarefa> tasks { get; set; } = new List<Tarefa>();
+
+
+
+
+    }
+}
